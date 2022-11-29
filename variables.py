61109d2c5246f836a7ece6832930c7863454f864
romain <r.quinion@gmail.com>
2022-11-29 13:25:13 +0100

    création de variables
    ajout de variables

--- variables.py
+++ variables.py
@@ -1,13 +1,12 @@
-# opérateur d'affectation = permet d'injecter une valeur dans une variable 
+# l'opérateur d'affectation  = permet d'injecter une valeur dans une variable
 
 my_number1 = 123
 my_number2 = -42
 
-print(my_number1)
-print(my_number2)
-
-my_text1 = "ceci est une chaîne de caractères"
-my_text2 =  'ceci est aussi une chaîne de caractères'
+my_text1 = "ceci est une chaine de caractère"
+my_text2 = "ceci est aussi une chaine de caractère"
 
 print(my_text1)
 print(my_text2)
+print(my_number1)
+print(my_number2)
