ebdded8e60839f85673603e9119f0e1b017ea4eb
romain <r.quinion@gmail.com>
2022-12-05 13:39:34 +0100

    acours sur les variables
    ajout d'un nouveau type de donnees et d'un exo

--- variables.py
+++ variables.py
@@ -1,12 +1,65 @@
 # l'opérateur d'affectation  = permet d'injecter une valeur dans une variable
-
+# integer
 my_number1 = 123
 my_number2 = -42
-
-my_text1 = "ceci est une chaine de caractère"
-my_text2 = "ceci est aussi une chaine de caractère"
-
-print(my_text1)
-print(my_text2)
 print(my_number1)
 print(my_number2)
+
+# float
+my_number3 = 3.14
+my_number4 = -2.71
+my_number5 = 0.0
+print(my_number3)
+print(my_number4)
+print(my_number5)
+
+#booleen
+my_boolean1 = True
+my_boolean2 = False
+print(my_boolean1)
+print(my_boolean2)
+
+#none valeur nulle
+my_none = None
+print(my_none)
+print(type(None))
+
+# type pour connaitre le type
+print(type(my_number1))
+print(type(my_number3))
+print(type(my_boolean1))
+
+#string chaine de caractère
+my_text1 = "ceci est une chaine de caractère"
+my_text2 = 'ceci est aussi une chaine de caractère'
+print(my_text1)
+print(my_text2)
+
+# \ caractère d'échappement
+# \n saut de ligne
+my_text3 = "ef\varv\nazga"
+
+# """ entrée est un saut de ligne 
+print(my_text3)
+my_text4 = """abc
+ezf
+fef"""
+print(my_text4)
+
+#permutation de la valeur des variables
+a = 123
+b = 42
+a,b = b, a
+print(a, b)
+
+a = 123
+b = 42
+c = a
+a = b
+b = c
+print(a,b)
+
+# varirante qui ne marche qu'avec des nombres
+# c = a + b
+# a = a - c
+# b = c - b
