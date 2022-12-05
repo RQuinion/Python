dbc8744bf4f535316b2d1d4a974e74eefe11592a
romain <r.quinion@gmail.com>
2022-12-05 15:57:09 +0100

    creation du fichier operators

---
+++ operators.py
@@ -0,0 +1,41 @@
+import random
+import math
+
+# = affectation
+foo = 123
+
+# + addition
+foo = 123+ 42
+foo = foo + 42 # ou foo += 42 (operateur d'incrementation)
+
+# - soustraction
+foo = foo - 42
+foo = foo - 42 # ou foo -= 42 (operateur de decrementation)
+
+# / division
+foo = 123 / 42
+print(foo)
+
+# // division entière
+foo = 123 // 42
+print(foo)
+
+# % modulo
+foo = 4 % 3
+foo = random.randint(1,100)
+print(foo % 2)
+
+# * multplication
+
+
+# ** puissance
+foo = 2**2
+foo = 2**3
+foo = 2**4
+print(foo)
+
+# ^ puissance mais pas en python
+# sqrt() racine carée 
+foo = math.sqrt(4)
+foo = 4 ** (1/2)
+print(foo)
